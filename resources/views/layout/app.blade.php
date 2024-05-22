041bc026efe4d735c78b5bc94800ddafdd5597e6
Edzer Dionido <jeddionido26@gmail.com>
2024-05-22 15:47:35 +0800

    add styling

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,8 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     <script src="https://cdn.tailwindcss.com"></script>
+
+    {{-- blade-formatter-disable --}}
+    <style type="text/tailwindcss">
+        .btn-success {
+            @apply rounded-md border px-1 py-1 bg-green-600 ring-2 ring-emerald-400 font-semibold text-neutral-300;
+        }
+
+        .link {
+            @apply underline text-black hover:text-blue-500;
+        }
+    </style>
+    {{-- blade-formatter-enable --}}
+
 </head>
-<body>
-    @yield('contents')
+<body class="container mx-auto mt-10 mb-10 max-w-lg">
+    <main>
+        @yield('content')
+    </main>
 </body>
 </html>
