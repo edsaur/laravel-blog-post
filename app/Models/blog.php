ac03ca71f719cab16190e9565b82cff2d5961e3e
Edzer Dionido <jeddionido26@gmail.com>
2024-05-23 11:34:22 +0800

    add a fillable

--- app/Models/blog.php
+++ app/Models/blog.php
@@ -5,7 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class blog extends Model
+class Blog extends Model
 {
+    protected $fillable = ['author', 'title', 'content'];
     use HasFactory;
 }
