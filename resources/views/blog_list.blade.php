e2a6c69c1a6c30b1362cf720b49e84abf3ef307e
Edzer Dionido <jeddionido26@gmail.com>
2024-05-23 11:31:31 +0800

    add something

--- resources/views/blog_list.blade.php
+++ resources/views/blog_list.blade.php
@@ -8,10 +8,18 @@
 <p class="text-lg font-semibold text-slate-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem, eius ducimus consequuntur id est suscipit, doloremque totam corrupti ipsum, esse quasi eos pariatur quae consectetur! Earum labore pariatur vero qui.</p>
 
 <div class="mt-3">
-    <a href="#" class="btn-success">Add new blog post!</a>
+    <a href="{{route('blogs.addBlog')}}" class="btn-success">Add new blog post!</a>
 
     <div class="mt-9">
-        <p class="font-bold"><a href="#" class="link">Title of the blog post</a></p>
+        @forelse ($blogs as $blog)
+        <p class="font-bold">
+            <a href="{{route('blogs.blog', ['blog' => $blog->id])}}" class="link">
+                {{$blog->id}}. {{$blog->title}}
+            </a>
+        </p>            
+        @empty
+            <h2>No records</h2>
+        @endforelse
     </div>
 </div>
 @endsection
