e4237c3f1ff9afd9e407e5d3882f29467b9f9d75
Edzer Dionido <jeddionido26@gmail.com>
2024-05-23 11:30:48 +0800

    add new processes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Requests\BlogRequest;
+use App\Models\Blog;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +20,21 @@ Route::get('/', function () {
     return redirect()->route('blogs.main');
 });
 
-Route::get('/blogs', function () {
-    return view('blog_list');
+Route::get('blogs', function () {
+    $blog = Blog::all();
+    return view('blog_list', ['blogs' => $blog]);
 })->name('blogs.main');
 
-Route::get('/blog/{blog}', function() {
+Route::get('blog/{blog}', function(Blog $blog) {
+    return view('blog', ['blog' => $blog]);
+})->name('blogs.blog');
+
+route::get('add-blog', function() {
+    return view('add-blog');
+})->name('blogs.addBlog');
+
+
+Route::post('add-blog', function (BlogRequest $request) {
+    $blog = Blog::create($request->validated());
     
 });
